4e9907a1728a75d599c0fcf657556e3db884f0f7
Abubakr <thefox5000@gmail.com>
2023-10-29 09:39:48 +0300

    Fix AC button layout issue

--- src/components/CalcButtons/index.jsx
+++ src/components/CalcButtons/index.jsx
@@ -4,35 +4,36 @@ export default function CalcButtons ({logic}) {
     return (
         <form className="button-body">
           <section className="">
-            <div className="row justify-content-center">
-                <button className="col-11 reset-button px-1" onClick={logic.resetSum}>AC</button>
+            <div className="row justify-content-between mx-1">
+                <button className="col-9 reset-button px-1" onClick={logic.resetSum}>AC</button>
+                <button className="button" value={'subtraction'} onClick={logic.subtract}>−</button>
             </div>
             <div className="row justify-content-between mx-1">
-                <button className="button number-button" onClick={logic.resetSum}>7</button>
-                <button className="button number-button" onClick={logic.resetSum}>8</button>
-                <button className="button number-button" onClick={logic.resetSum}>9</button>
-                <button className="button" onClick={logic.divide}>/</button>
+                <button className="button number-button" value={7} onClick={logic.addNumber}>7</button>
+                <button className="button number-button" value={8} onClick={logic.addNumber}>8</button>
+                <button className="button number-button" value={9} onClick={logic.addNumber}>9</button>
+                <button className="button" value={'division'} onClick={logic.divide}>/</button>
             </div>
             <div className="row justify-content-between mx-1 my-2">
-                <button className="button number-button" onClick={logic.resetSum}>4</button>
-                <button className="button number-button" onClick={logic.resetSum}>5</button>
-                <button className="button number-button" onClick={logic.resetSum}>6</button>
-                <button className="button" onClick={logic.multiply}>X</button>
+                <button className="button number-button" value={4} onClick={logic.addNumber}>4</button>
+                <button className="button number-button" value={5} onClick={logic.addNumber}>5</button>
+                <button className="button number-button" value={6} onClick={logic.addNumber}>6</button>
+                <button className="button" value={'multiplication'} onClick={logic.multiply}>X</button>
             </div>
             <div className="row justify-content-between mx-1 my-2">
-                <button className="button number-button" onClick={logic.resetSum}>1</button>
-                <button className="button number-button" onClick={logic.resetSum}>2</button>
-                <button className="button number-button" onClick={logic.resetSum}>3</button>
-                <button className="button" onClick={logic.add}>+</button>
+                <button className="button number-button" value={1} onClick={logic.addNumber}>1</button>
+                <button className="button number-button" value={2} onClick={logic.addNumber}>2</button>
+                <button className="button number-button" value={3} onClick={logic.addNumber}>3</button>
+                <button className="button" value={'addition'} onClick={logic.makeOperation}>+</button>
             </div>
             <div className="row justify-content-between mx-1 my-2">
-                <button className="zero-button" onClick={logic.resetSum}>0</button>
-                <button className="button number-button" onClick={logic.resetSum}>.</button>
+                <button className="zero-button" value={0} onClick={logic.addNumber}>0</button>
+                <button className="button number-button" onClick={logic.addNumber}>.</button>
                 <button className="button" onClick={logic.add}>=</button>
             </div>
             {/* <input value={logic.input} onChange={logic.handleChange} type="number" /> */}
             
-            {/* <button className="button" onClick={logic.subtract}>−</button> */}
+            
             
             {/* <button className="button" onClick={resetInput}>RI</button> */}
           </section>
--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -9,10 +9,12 @@ import CalcButtons from "../CalcButtons";
 
 export default function Calc() {
   const [input, setInput] = useState(0);
+  const [sum, setSum] = useState(0);
+  const [operation, setOperation] = useState(null);
+
   const handleChange = (e) => {
     setInput(e.target.value);
   };
-  const [sum, setSum] = useState(0);
 
   const add = (evt) => {
     evt.preventDefault();
@@ -24,6 +26,9 @@ export default function Calc() {
 
   const subtract = (evt) => {
     evt.preventDefault();
+    if(sum) {
+      setSum(previousSum => String())
+    }
     setSum(Number(sum) - Number(input));
     setInput(0);
   };
@@ -53,12 +58,31 @@ export default function Calc() {
     setInput((previousSum) => (previousSum = 0));
   };
 
+  const addNumber = (evt) => {
+    evt.preventDefault();
+    setSum(previousSum => Number(String(previousSum) + String(evt.target.value)));
+  } 
+
+  const makeOperation = (evt) => {
+    evt.preventDefault();
+    const currentOperation = evt.target.value;
+    if(currentOperation === '+') {
+      console.log('This is addition');
+    } else if(currentOperation === '+') {
+      console.log('This is addition');
+    } else if(currentOperation === '+') {
+      console.log('This is addition');
+    } else if(currentOperation === '+') {
+      console.log('This is addition');
+    }
+  } 
+
   return (
     <>
       <section className="calc-body">
       <TopBar />
       <Display result={sum}/>
-      <CalcButtons logic={{input, handleChange, add, subtract, multiply, divide, resetSum, resetInput}}/>
+      <CalcButtons logic={{makeOperation, addNumber, input, handleChange, add, subtract, multiply, divide, resetSum, resetInput}}/>
       </section>
     </>
   );
